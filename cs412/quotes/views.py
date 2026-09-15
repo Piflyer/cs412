b3d597e371ba7e7857e4c361bc2a9b781ade5089
Tim Nguyen <timnguyen737@gmail.com>
2026-09-14 22:32:34 -0400

    Cleaning up repo and adding in test for static deployment

--- cs412/quotes/views.py
+++ cs412/quotes/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
 def quote(request):
-    pass
+    '''renders the quotes in a random fashion'''
+    
+    response_text = '''
+    <html>
+    <h1>Quote!</h1>
+    </html>
+    '''
+    return HttpResponse(response_text)
 
 def show_all(request):
-    pass
+    '''shows all the quotes'''
+    
+    response_text = '''
+    <html>
+    <h1>All Quotes</h1>
+    </html>
+    '''
+    return HttpResponse(response_text)
 
 def about(request):
-    pass
+    '''about page'''
+    
+    response_text = '''
+    <html>
+    <h1>About</h1>
+    </html>
+    '''
+    return HttpResponse(response_text)
