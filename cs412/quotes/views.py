7f024244e9937fa845f7e878a592c724d2ad917e
Tim Nguyen <timnguyen737@gmail.com>
2026-09-14 23:33:15 -0400

    Added in list of images to render and have the base navigation as well as quote sites up and live

--- cs412/quotes/views.py
+++ cs412/quotes/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+import random
 
 # Create your views here.
 
+Quotes = [
+    "I got something I want to talk about - Madonna",
+    "Competition is for losers - Peter Thiel",
+    "My goal is YC - Every larper out there"
+]
+
+Images = [
+    "https://pics.freeartbackgrounds.com/midle/Nature_Landscape_Background-223.jpg",
+    "https://pics.freeartbackgrounds.com/midle/Azure_Blue_Sea_Background-775.jpg",
+    "https://pics.freeartbackgrounds.com/midle/Eiffel_Tower_at_Night_Paris_Background-1289.jpg",
+]
+
 def quote(request):
-    '''renders the quotes in a random fashion'''
+    '''renders the quotes in a random fashion, goes through a list of images and quotes and randomly picks them'''
+
     
-    response_text = '''
-    <html>
-    <h1>Quote!</h1>
-    </html>
-    '''
-    return HttpResponse(response_text)
+    context = {
+        'quote': random.choice(Quotes),
+        'image': random.choice(Images),
+    }
+    return render(request, 'quotes/quote.html', context)
 
 def show_all(request):
     '''shows all the quotes'''
