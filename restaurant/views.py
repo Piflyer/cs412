a6c0396e48ea6f26163b6c676df1c0893851c1df
Tim Nguyen <timnguyen737@gmail.com>
2026-09-21 13:02:44 -0400

    list of menu items updated

--- restaurant/views.py
+++ restaurant/views.py
@@ -7,22 +7,30 @@ def main(request):
 
 def order(request):
     
-    daily_specials = {"Chip Shortage - $4.99" : "Extra yummy chips that will definitely leave you with shortage when paired with our not-quite-queso-queso dip.",
-                      "Stackoverflow Nachos - $14.99" : "Our take on a nacho tower, paired with our house-made chips and organic ground beef. You'll clear the stack, guaranteed.",
-                      "Hash-table Browns - $3.99": "Our take on a hash brown, except faster and better."}
     
-    core_menu = {
-        "Spam & Eggs - $12.99" : "The breakfast you never asked for, yet you see it everywhere.",
-        "Not-a-Bubble Bubble Milkshake - $7.99" : "We are not in a bubble, but we still made a bubble milkshake.",
-        "Bacon-developer - $14.99" : "We used some very delicate logic to sizzle and serve this to perfection.",
-        "Localhost Fries - $5.99" : "No place like 127.0.0.1. And some fries to go with it.",
-        "Phish Tacos - $12.99" : "Looks suspiciously authentic and good, you will get hooked.",
-        "Raspberry Pi - $8.99" : "Nothing completes a meal like a low cost, bite-sized desert."
+    daily_specials= [
+        ["Chip Shortage", 4.99, "Extra yummy chips that will definitely leave you with shortage when paired with our not-quite-queso-queso dip.", []],
+        ["Stackoverflow Nachos", 14.99, "Our take on a nacho tower, paired with our house-made chips and organic ground beef. You'll clear the stack, guaranteed.", []],
+        ["Hash-table Browns", 3.99, "Our take on a hash brown, except faster and better.", []]
+    ]
+    
+    core_menu= [
+        ["Spam & Eggs", 12.99, "The breakfast you never asked for, yet you see it everywhere", []],
+        ["Not-a-Bubble Bubble Milkshake", 7.99, "We are not in a bubble, but we still made a bubble milkshake", []],
+        ["Bacon-developer", 14.99, "We used some very delicate logic to sizzle and serve this to perfection.", []],
+        ["Localhost Fries", 5.99, "No place like 127.0.0.1. And some fries to go with it.", [["MegaBite", 0], ["GigaBite", 1], ["TerraBite", 2]]],
+        ["Bacon-developer", 14.99, "We used some very delicate logic to sizzle and serve this to perfection.", []],
+        ["Phish Tacos", 12.99, "Looks suspiciously authentic and good, you will get hooked.", []],
+        ["Raspberry Pi", 8.99, "Nothing completes a meal like a low cost, bite-sized desert.", []]
+    ]
+    
+    context = {
+        'special' : random.choice(daily_specials),
+        'core_menu' : core_menu
     }
     
     
-    
-    return render(request, 'restaurant/main.html')
+    return render(request, 'restaurant/order.html', context=context)
 
 def confirmation(request):
     return render(request, 'restaurant/main.html')
